ab04ced736efb8fef119cbf884388d10cd72ed3d
AleHashirama <alehashirama@gmail.com>
2024-04-02 16:20:06 +0200

    Bonus 1

--- index.php
+++ index.php
@@ -4,19 +4,22 @@ class Movie {
     public $title;
     public $plot;
     public $year;
-    
+    public $genres;
+
     /**
      * __construct
      *
      * @param  string $_title
      * @param  string $_plot
      * @param  int $_year
+     * @param  array $_genres
      */
     
-    public function __construct($_title, $_plot, $_year) {
+    public function __construct($_title, $_plot, $_year, $_genres) {
         $this->title = $_title;
         $this->plot = $_plot;
         $this->year = $_year;
+        $this->genres = $_genres;
     }
 
     public function showInfo() {
@@ -25,13 +28,18 @@ class Movie {
         echo "Trama: {$this->plot}";
         echo "<br>";
         echo "Anno: {$this->year}";
+        echo "<br>";
+        echo "Generi: ";
+        foreach ($this->genres as $genre) {
+            echo $genre;
+        }
     }
 }
 
-$movie1 = new Movie("Il Labirinto del Fauno", "Spagna, 1944. L'esercito sta piegando le ultime frange di resistenza e la piccola Ofelia si rifugia in un realistico mondo delle fiabe...", 2006);
-$movie2 = new Movie("Pinocchio", "Il desiderio di un padre porta magicamente in vita un bambino di legno in Italia, dandogli la possibilità di prendersene cura.", 2022);
-$movie3 = new Movie("Beetlejuice", "Una coppia di felici coniugi fantasmi è alle prese con una famiglia di chiassosi e petulanti snob che hanno occupato la loro casa.", 1988);
-$movie4 = new Movie("Beetlejuice 2", "Lo spiritello porcello è tornato per un esilarante e irriverente sequel. In questo capitolo, il fantasma reclutato per aiutare a infestare una casa condividerà un'avventura con la figlia di Lydia.", 2024);
+$movie1 = new Movie("Il Labirinto del Fauno", "Spagna, 1944. L'esercito sta piegando le ultime frange di resistenza e la piccola Ofelia si rifugia in un realistico mondo delle fiabe...", 2006, ["Fantasy","/","War"]);
+$movie2 = new Movie("Pinocchio", "Il desiderio di un padre porta magicamente in vita un bambino di legno in Italia, dandogli la possibilità di prendersene cura.", 2022, ["Fantasy"]);
+$movie3 = new Movie("Beetlejuice", "Una coppia di felici coniugi fantasmi è alle prese con una famiglia di chiassosi e petulanti snob che hanno occupato la loro casa.", 1988, ["Comedy","/","Horror"]);
+$movie4 = new Movie("Beetlejuice 2", "Lo spiritello porcello è tornato per un esilarante e irriverente sequel. In questo capitolo, il fantasma reclutato per aiutare a infestare una casa condividerà un'avventura con la figlia di Lydia.", 2024, ["Comedy","/","Horror"]);
 
 ?>
 
